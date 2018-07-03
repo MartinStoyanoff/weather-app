d15ef9cf9f851e424484b9d08b13b9c813903feb
Martin Ivanov <stoyanoff.martin@gmail.com>
2018-07-03 22:39:35 +0300

    Js moved to different files

---
+++ JavaScript/autoComplete.js
@@ -0,0 +1,4 @@
+function activatePlacesSearch() {
+    var input = document.getElementById('city');
+    var autocomplete = new google.maps.places.Autocomplete(input);
+}
---
+++ JavaScript/search.js
@@ -0,0 +1,46 @@
+$(document).ready(function () {
+    var city = $("#city").val();
+
+    open_weather(city);
+});
+$(document).ready(function () {
+    $('#city').bind('keyup blur', function () {
+        var node = $(this);
+        node.val(node.val());
+    });
+    $("#clk").click(function () {
+        var city = $("#city").val();
+        //alert(city);
+        open_weather(city);
+    });
+
+
+
+});
+
+function open_weather(city) {
+
+    $('.weather-temperature').openWeather({
+        key: '13637fc49e69ecb7364b52deebe17422',
+        city: city,
+        descriptionTarget: '.weather-description',
+        windSpeedTarget: '.weather-wind-speed',
+        pressureTarget: '.weather-pressure',
+        humidityTarget: '.weather-humidity',
+        sunriseTarget: '.weather-sunrise',
+        cloudsTarget: '.weather-clouds',
+        sunsetTarget: '.weather-sunset',
+        placeTarget: '.weather-place',
+        iconTarget: '.icon-target',
+        success: function () {
+            //show weather
+            $('.weather-wrapper').fadeIn(800);
+
+        },
+        error: function (message) {
+            console.log(message);
+            $('.weather-wrapper').fadeOut(1000)
+            alert("Enter City Name Properly.");
+        }
+    });
+}
